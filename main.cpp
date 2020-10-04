52a2565dcb0ead5ab555816c937892677f9f4935
Viktor <kiva@list.ru>
2020-10-04 16:20:20 +0300

    DONE

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-#include<iostream>
-using namespace std;
-#define tab "\t"
-void FillRand(int arr[], const int n);
-void Print(int arr[], const int n);
+#include"Function.h"
+#include"Function.cpp"
 
 void main()
 {
@@ -16,21 +13,6 @@ void main()
 
  }
 
-void FillRand(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-    }
-}
-
-void Print(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << tab;
-	}
-}
 
 
 
